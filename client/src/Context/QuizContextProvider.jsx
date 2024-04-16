c97e5c4d00d42d8aeecf38bc36af63db25fac51f
prabhat-4165 <prabhu27082001@gmail.com>
2024-04-16 11:33:29 +0530

    Added Contest Apis

---
+++ client/src/Context/QuizContextProvider.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import QuizContext from "./QuizContext";
+const QuizContextProvider = ({ children }) => {
+  const [form, setForm] = useState(null);
+  return <QuizContext.Provider value={{form, setForm}}>{children}</QuizContext.Provider>;
+};
+
+export default QuizContextProvider;
