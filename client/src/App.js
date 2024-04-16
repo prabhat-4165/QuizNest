8f277f8031eca8a4ce42144efb12b16b68637635
prabhat-4165 <prabhu27082001@gmail.com>
2024-04-16 11:34:03 +0530

    updated navigating from landing page

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,19 @@ import HomeNew from './Components/LandingPage/HomeNew';
 import About from './Components/LandingPage/About';
 import Contact from './Components/LandingPage/Contact';
 import LoginContextProvider from './Context/LoginContextProvider';
+import UserQuizContextProvider from './Context/UserQuizContextProvider';
+import QuizContextProvider from './Context/QuizContextProvider';
+import CreateQuiz from './Components/QuizCreation/CreateQuiz';
+import QuizContextProvider from './Context/QuizContextProvider';
+import UserDash from './Components/UserDashboard/UserDash';
+import AdminQuizDetail from './Components/AdminDashboard/AdminQuizDetail';
+import Instruction from './Components/UserDashboard/Instruction';
+import QuizTest from './Components/UserQuiz/QuizTest';
+import Submitted from './Components/UserQuiz/Submitted';
+import Result from './Components/Results/Result';
+import Leaderboard from './Components/Results/Leaderboard';
+import About from './Components/LandingPage/About';
+import Contact from './Components/LandingPage/Contact';
 
 function App() {
   // const [currentForm,serCurrentForm]=useState('login');
@@ -19,6 +32,16 @@ function App() {
         <Route index path='/' element={<HomeNew />}></Route>
         <Route path='/register' element={<Register />}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='/about' element={<About />}></Route>
+        <Route path='/contact' element={<Contact />}></Route>
+        <Route path='/custom-quiz' element={<QuizContextProvider><CreateQuiz /></QuizContextProvider>}></Route>
+        <Route path='/user' element={<UserDash />}></Route>
+        <Route path='/detail-quiz' element={<AdminQuizDetail />}></Route>
+        <Route path='/instruction' element={<Instruction />}></Route>
+        <Route path='/quiz' element={ <UserQuizContextProvider><QuizTest /></UserQuizContextProvider>}></Route>
+        <Route path='/submitted' element={<Submitted />}></Route>
+        <Route path='/result' element={<Result />}></Route>
+        <Route path='/leaderboard' element={<Leaderboard />}></Route>
         <Route path='/about' element={<About />}></Route>
         <Route path='/contact' element={<Contact />}></Route>
       </Routes>
