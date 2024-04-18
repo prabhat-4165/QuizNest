b5c88b3d952e68d1edb85ea51b1876e659fa907e
puneet2003 <puneetkumar322003@gmail.com>
2024-04-18 22:47:40 +0530

    added context apis

--- client/src/Context/LoginContextProvider.jsx
+++ client/src/Context/LoginContextProvider.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import LoginContext from "./LoginContext";
+
 const LoginContextProvider = ({children}) => {
     const [loginId, setloginId] = useState(null);
+    // send loginId and method 
     return <LoginContext.Provider value={{loginId, setloginId}}>{children}</LoginContext.Provider>
 }
 
--- client/src/Context/QuizContextProvider.jsx
+++ client/src/Context/QuizContextProvider.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import QuizContext from "./QuizContext";
 const QuizContextProvider = ({ children }) => {
   const [form, setForm] = useState(null);
+  
   return <QuizContext.Provider value={{form, setForm}}>{children}</QuizContext.Provider>;
 };
 
