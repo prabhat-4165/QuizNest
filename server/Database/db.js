9d01a864469f2097785697fd1fcebdb8b5306b5b
puneet2003 <puneetkumar322003@gmail.com>
2024-10-08 04:01:10 +0530

    Resolved merge conflict in Connection.js

--- server/Database/db.js
+++ server/Database/db.js
@@ -1,8 +1,13 @@
 const User = require("../Models/User")
 const mongoose = require("mongoose");
 const Connection = async () => {
+<<<<<<< HEAD
     const URL = "mongodb://127.0.0.1:27017/Puneet";
     // const URL = "mongodb+srv://triprabhat__001:nXDTCmoV8gNammwq@quiz-app.tckswf5.mongodb.net";
+=======
+    //const URL = "mongodb://127.0.0.1:27017/QuizNest";
+    const URL = "mongodb+srv://triprabhat__001:nXDTCmoV8gNammwq@quiz-app.tckswf5.mongodb.net";
+>>>>>>> 1b7c282f41ff34419006ec2fd72f7a44f18eac41
     try {
         const db = await mongoose.connect(URL);
         console.log('Database connected successfully');
